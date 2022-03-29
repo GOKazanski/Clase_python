5f5124d4232bad35e4a8bfba033108f480cd73ba
Gabriel Omar Kazanski <90653565+GOKazanski@users.noreply.github.com>
2022-03-29 15:49:19 -0300

    Update TP_n_01.py

--- TP_n_01.py
+++ TP_n_01.py
@@ -43,4 +43,32 @@ print (usar_la_fuerza (mochila_jedi, 0)
 # Se comenzará en la casilla (0, 0) y se termina en la (n-1, n-1). Se mueve a la siguiente casilla si es posible, 
 # cuando no se pueda avanzar hay que retroceder sobre los pasos dados en busca de un camino alternativo.
 
+laberinto = [[1,0,1,1,0,1,1,1,1,1],
+             [1,1,1,0,1,1,0,0,0,1],
+             [0,0,1,1,1,0,0,0,1,1],
+             [0,0,0,0,0,1,1,1,0,1],
+             [1,1,1,1,1,1,0,1,1,1],
+             [1,0,0,0,0,0,0,0,0,0],
+             [1,0,1,0,0,1,1,1,1,1],
+             [1,0,1,1,1,1,0,0,0,1],
+             [1,0,0,1,0,0,0,1,1,1],
+             [1,1,1,1,0,2,1,1,0,1]]
+       
+def salida_del_laberinto (matriz, x, y, recorrido=[]):
+       if (x>=0 and x<= len (matriz)-1) and (y>=0 and y<= len(matriz[0])-1):
+            if (matriz [x][y]==2):
+            recorrido . append ([x,y])
+            print ('ha encontrado la salida del laberinto')
+            print (recorrido)
+            recorrido . pop ()
+       elif (matriz [x][y]==1):
+            matriz [x][y]=3
+            recorrido . append ([x,y])
+            salida_del_laberinto (matriz, x, y+1, recorrido)
+            salida_del_laberinto (matriz, x, y-1, recorrido)       
+            salida_del_laberinto (matriz, x+1, y, recorrido)       
+            salida_del_laberinto (matriz, x-1, y, recorrido)       
+            recorrido . pop ()
+            matriz [x][y]=1
 
+print ( salida_del_laberinto(laberinto, 0, 0)
