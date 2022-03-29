b02cd00f46a63443094348c8e920a2d615121f9d
Gabriel Omar Kazanski <90653565+GOKazanski@users.noreply.github.com>
2022-03-29 15:08:51 -0300

    Update TP_n_01.py

--- TP_n_01.py
+++ TP_n_01.py
@@ -13,9 +13,8 @@ def convertir_romano_a_decimal (num, pos, n_romanos):
         else:
             if (romana_actual >= ronana_siguiente):
                 return romana_actual + convertir_romano_a_decimal (num, pos + 1, n_romanos)
+            
 print (convertir_romano_a_decimal ('MCMLXXIV', 0, n_romanos))
-
-
 
 # 22 - El problema de la mochila Jedi. Suponga que un Jedi (Luke Skywalker, Obi-Wan Kenobi, Rey u 
 # otro, el que más le guste) está atrapado, pero muy cerca está su mochila que contiene muchos objetos. 
@@ -26,10 +25,19 @@ print (convertir_romano_a_decimal ('MCMLXXIV', 0, n_romanos))
 # b. determinar si la mochila contiene un sable de luz y cuantos objetos fueron necesarios sacar para encontrarlo; 
 # c. Utilizar un vector para representar la mochila.
 
+mochila_jedi = ['manta', 'casco','escudo','sable de luz', 'kimono']
 
-
-
-
+def usar_la_fuerza (mochila:_jedi, pos):
+    if (pos < len(mochila_jedi):
+        if (mochila_jedi [pos]=='sable de luz'):
+            print ('El Sable de Luz salió estaba en la mochila y salió en la posición: ', pos)
+        else:
+            return usar_la_fuerza (mochila_jedi, pos + 1)
+    else:
+        print ('El sable de luz no está en la mochila')
+        
+print (usar_la_fuerza (mochila_jedi, 0)
+        
 # 23 - Salida del laberinto. Encontrar un camino que permita salir de un laberinto definido en una matriz de [n x n], 
 # solo se puede mover de a una casilla a la vez –no se puede mover en diagonal– y que la misma sea adyacente y no esté marcada como pared. 
 # Se comenzará en la casilla (0, 0) y se termina en la (n-1, n-1). Se mueve a la siguiente casilla si es posible, 
